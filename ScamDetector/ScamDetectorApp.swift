f0b745d8c3a2fa21e45bf096528a357cc9e6db38
Ivan Ishchuk <162058292+g0dhelpme@users.noreply.github.com>
2026-05-23 17:38:14 +0200

    Initial Commit

---
+++ ScamDetector/ScamDetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScamDetectorApp.swift
+//  ScamDetector
+//
+//  Created by Ivan Ishchuk on 23.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct ScamDetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
